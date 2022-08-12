f119a3dabb28146c0b24588168619995fc84d47f
3alte3 <87598968+3alte3@users.noreply.github.com>
2022-08-12 10:45:23 +0300

    Add files via upload

--- showroom/serializers.py
+++ showroom/serializers.py
@@ -1,27 +1,24 @@
 from rest_framework import serializers
 from django_countries.serializer_fields import CountryField
+from .models import *
 
-class ShowroomSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=255)
-    content = serializers.CharField(max_length=255)
-    balance = serializers.IntegerField()
-    location = CountryField()
-    showroom_id = serializers.IntegerField()
-    isActive = serializers.BooleanField(default=True)
+class ShowroomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = showroom
+        fields = ('id',"name","content","balance","location","showroom_id")
 
-class Charact_ShowroomSerializer(serializers.Serializer):
-    engine_type = serializers.CharField(max_length=255)
-    max_speed = serializers.CharField(max_length=255)
-    ammount_of_eng = serializers.IntegerField()
-    model = serializers.CharField(max_length=255)
-    mark = serializers.CharField(max_length=255)
-    showroom_id = serializers.IntegerField()
 
-class CarsSerializer(serializers.Serializer):
-    model = serializers.CharField(max_length=255)
-    mark = serializers.CharField(max_length=255)
-    price = serializers.IntegerField()
-    showroom_id = serializers.IntegerField()
+class Charact_ShowroomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = charact_showroom
+        fields = ('engine_type','max_speed','ammount_of_eng','model','mark','showroom_id',)
+
+
+class CarsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = cars
+        fields = ('id','engine_type','max_speed','ammount_of_eng','model','mark','price','showroom_id','ammount_of_cars',)
+
 
 class HistorySerializer(serializers.Serializer):
     date = serializers.DateTimeField()
@@ -29,6 +26,7 @@ class HistorySerializer(serializers.Serializer):
     price = serializers.IntegerField()
     showroom_id = serializers.IntegerField()
 
-class UniqueBuyersSerializer(serializers.Serializer):
-    buyer_id = serializers.IntegerField()
-    showroom_id = serializers.IntegerField()
+class UniqueBuyersSerializer(serializers.ModelSerializer):
+    class Meta :
+        model = unique_buyers
+        fields = ("buyer_id","showroom_id","amount_of_purchase")
