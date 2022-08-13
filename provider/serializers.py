80235fe2ce30e9dcbd85c375d1f3f0068c9910b3
3alte3 <87598968+3alte3@users.noreply.github.com>
2022-08-13 16:35:47 +0300

    Added sales algorithm
    Добавлен алгоритм выбора наилучшего предложения , исправлено добавление в историю provider

--- provider/serializers.py
+++ provider/serializers.py
@@ -2,13 +2,18 @@ from rest_framework import serializers
 from django_countries.serializer_fields import CountryField
 from .models import *
 
-class ProviderAPIViewSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=255)
-    date = serializers.DateTimeField()
-    ammountOfBuyers = serializers.IntegerField()
-    providerId = serializers.IntegerField()
+class ProviderAPIViewSerializer(serializers.ModelSerializer):
+    class Meta :
+        model = provider
+        fields = ("name","date","ammountOfBuyers","providerId",)
+
 
 class ListOfCarsAPIViewSerializer(serializers.ModelSerializer):
     class Meta:
         model=listOfCars
         fields =('id',"engine_type","max_speed","ammount_of_eng","model","mark","price","providerId",)
+
+class ProvideHistoryAPIViewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=providerHistory
+        fields =('date',"providerId","showroom_id","model","mark")
--- provider/views.py
+++ provider/views.py
@@ -12,24 +12,30 @@ from django.utils import timezone
 from django.shortcuts import render
 
 class ProviderAPIView(APIView):
+    queryset = provider.objects.all()
+    serializer_class = ListOfCarsAPIViewSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['name']
+    search_fields = ['providerId', 'name']
     permission_classes = (IsAuthenticated,)
-    def get(self, request):
-        lst = provider.objects.all()
-        return Response({'posts': ProviderAPIViewSerializer(lst, many=True).data})
 
-    def post(self, request):
-        post_new = provider.objects.create(
-            name=request.data['name'],
-            date=timezone.now(),
-            ammountOfBuyers=request.data['ammountOfBuyers'],
-            providerId = request.data['providerId'],
-        )
-        return Response({'post': ProviderAPIViewSerializer(post_new).data})
-
-    def delete(self,request):
-        postToDel = provider.objects.get(name = request.data['name'],providerId = request.data['providerId'])
-        postToDel.delete()
-        return Response({'deleted' : ProviderAPIViewSerializer(postToDel).data})
+    # def get(self, request):
+    #     lst = provider.objects.all()
+    #     return Response({'posts': ProviderAPIViewSerializer(lst, many=True).data})
+    #
+    # def post(self, request):
+    #     post_new = provider.objects.create(
+    #         name=request.data['name'],
+    #         date=timezone.now(),
+    #         ammountOfBuyers=request.data['ammountOfBuyers'],
+    #         providerId = request.data['providerId'],
+    #     )
+    #     return Response({'post': ProviderAPIViewSerializer(post_new).data})
+    #
+    # def delete(self,request):
+    #     postToDel = provider.objects.get(name = request.data['name'],providerId = request.data['providerId'])
+    #     postToDel.delete()
+    #     return Response({'deleted' : ProviderAPIViewSerializer(postToDel).data})
 
 
 class ListOfCarsViewSet(viewsets.ModelViewSet):
@@ -39,5 +45,14 @@ class ListOfCarsViewSet(viewsets.ModelViewSet):
     filterset_fields = ['price']
     search_fields = ['model', 'mark']
     permission_classes = (IsAuthenticated,)
+
+class ProviderHistoryViewSet(viewsets.ModelViewSet):
+    queryset = providerHistory.objects.all()
+    serializer_class = ProvideHistoryAPIViewSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['model']
+    search_fields = ['model', 'mark']
+    permission_classes = (IsAuthenticated,)
+
 
 
